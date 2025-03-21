33648bc1f9db23773bb253500c8276ae08b8b9b5
LautyMCI <lautaromanucci54@gmail.com>
2025-03-21 01:06:44 -0300

    Solución a bug para respuestas sin numeros o fuera de rango

--- Practica 1.py
+++ Practica 1.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 # Preguntas para el juego
 questions = [
@@ -36,14 +37,20 @@ for _ in range(3):
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
+        user_answer = input("Respuesta: ")
+        # Se verifica si la respuesta es valida
+        if user_answer in ["1","2","3","4"]:
+            user_answer = int(user_answer) - 1
+        else:
+            print("Respuesta no válida")
+            sys.exit(1)
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
             break
     else:
         # Si el usuario no responde correctamente después de 2 intentos,
-        # se muestra la respuesta correcta
+        # Se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index][correct_answers_index[question_index]])
 
